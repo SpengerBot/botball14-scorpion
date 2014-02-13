d400596a4e35854f33885fe63381775acd4d17e1
Michael Kaufmann <michi.kaufmann1012@gmail.com>
2014-02-13 22:07:08 +0100

    First programm

---
+++ firstcreateprogramm.c
@@ -0,0 +1,43 @@
+
+void turn_right();
+void turn_left();
+#define cubeClaw 0
+#define botguyClaw 1
+#define leftHook 2
+#define rightHook 3
+#define hookDown 750
+#define hookUp 1450
+#define motor 1 
+#define speed 500
+
+
+void main(){
+	enable_servos();
+	set_servo_position(leftHook, hookDown);
+	set_servo_position(rightHook, hookDown);
+	create_connect();
+	create_full();
+	turn_left();
+	set_create_distance(0);
+	create_drive_straight(speed);
+	while(get_create_distance()<100){
+	}
+	printf("done");
+	
+}
+
+void turn_right() {
+set_create_normalized_angle(0);
+	create_spin_CCW(100);
+	while(get_create_distance()<90){
+	}		
+}
+
+void turn_left() {
+set_create_normalized_angle(0);
+	create_spin_CW(100);
+	while(get_create_distance()<90){
+	}		
+}
+	
+
