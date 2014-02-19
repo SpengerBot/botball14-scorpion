e7a61a76d327739de2e50f1ca20e03bdd738f7e0
Alexander Halbarth <alexander@halbarth.at>
2014-02-19 16:22:36 +0100

    Create Commands (Drive&Rotate) funktionieren wait ausgebessert mit berechneter benötigter Zeit

--- create/Drive.c
+++ create/Drive.c
@@ -1,6 +1,4 @@
 void drive(int dist, int speed) {  //dist in mm, speed in mm/s   invertiert fuer create roboter
-	dist = dist * -1;
-	speed = speed * -1;
 	create_write_byte(152);        // specifies start of script definition
 	create_write_byte(13);         // specifies number of bytes to follow,(defining the script)
 	create_write_byte(137);        // drive command (speed and turn radius in next 4 bytes)
@@ -20,13 +18,14 @@ void drive(int dist, int speed) {  //dist in mm, speed in mm/s   invertiert fuer
 }
 
 void drive_wait(int dist, int speed) {  //dist in mm, speed in mm/s   invertiert fuer create roboter
-	drive(int dist, int speed);
-	while(get_create_distance()!=0){} //Warten bis create fertig ist
+	drive(dist, speed);
+	msleep(dist/speed*1000);
 }
 
 void rotate(int speed, int angle) {  //angle: positiv --> rechts     negativ --> links
 	int rad = 0xFFFF;
-	if(angle<0) {
+	angle = angle * -1;
+	if(angle>0) {
 		rad = 0x0001;
 	}
 	create_write_byte(152);        // specifies start of script definition
@@ -34,11 +33,11 @@ void rotate(int speed, int angle) {  //angle: positiv --> rechts     negativ -->
 	create_write_byte(137);        // drive command (speed and turn radius in next 4 bytes)
 	create_write_byte(speed >> 8); // send speed high byte (bits 8-15 shifted to 0-7)
 	create_write_byte(speed);      // send speed low byte
-	create_write_byte(rad >> 8);        // send radius high byte (bits 8-15 shifted to 0-7)
-	create_write_byte(rad);          // send radius low byte
+	create_write_byte(rad >> 8);   // send radius high byte (bits 8-15 shifted to 0-7)
+	create_write_byte(rad);        // send radius low byte
 	create_write_byte(157);        // wait for distance done (in mm)
-	create_write_byte(angle >> 8);  // send dist high byte
-	create_write_byte(angle);       // send dist low byte
+	create_write_byte(angle >> 8); // send dist high byte
+	create_write_byte(angle);      // send dist low byte
 	create_write_byte(137);        // stop move by changing speed and radius to 0
 	create_write_byte(0);          // send high byte (0 speed)
 	create_write_byte(0);          // send low byte (0 speed)
@@ -48,6 +47,6 @@ void rotate(int speed, int angle) {  //angle: positiv --> rechts     negativ -->
 }
 
 void rotate_wait(int speed, int angle) {
-	rotate(int speed, int angle);
-	while(get_create_normalized_angle()!=0){} //Warten bis create fertig ist
+	rotate(speed, angle);
+	msleep(2268.9280275*angle/speed); //d*pi*angle/360		(d=260mm durchmesser rad zu rad) (260*pi/360=2,2689280275926284500007979990352 *1000=2268,9280275926284500007979990352 (genauigkeit 11 Stellen))
 }
