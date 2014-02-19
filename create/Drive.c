b1087c7f4c2c148913e9017745bbfa71321a918c
Alexander Halbarth <alexander@halbarth.at>
2014-02-19 11:11:40 +0100

    Create Scripting hinzugefügt und verwendet UNTESTED!

--- create/Drive.c
+++ create/Drive.c
@@ -1,43 +1,3 @@
-#define RUN_SCRIPT create_write_byte(153) // macro to run the currently loaded script
-void make_drive_script(int dist, int speed) {  //dist in mm, speed in mm/s
-	create_write_byte(152);        // specifies start of script definition
-	create_write_byte(13);         // specifies number of bytes to follow,(defining the script)
-	create_write_byte(137);        // drive command (speed and turn radius in next 4 bytes)
-	create_write_byte(speed >> 8); // send speed high byte (bits 8-15 shifted to 0-7)
-	create_write_byte(speed);      // send speed low byte
-	create_write_byte(128);        // send hex 80
-	create_write_byte(0);          // send hex 00 (special case: turn radius hex 8000 or 7FFF is straight)
-	create_write_byte(156);        // wait for distance done (in mm)
-	create_write_byte(dist >> 8);  // send dist high byte
-	create_write_byte(dist);       // send dist low byte
-	create_write_byte(137);        // stop move by changing speed and radius to 0
-	create_write_byte(0);          // send high byte (0 speed)
-	create_write_byte(0);          // send low byte (0 speed)
-	create_write_byte(0);          // null turn radius
-	create_write_byte(0);          // null turn radius
-	// end of script (13 bytes)
-}
-
-void drive_old(int dist, int speed) {  //dist in mm, speed in mm/s
-	create_write_byte(152);        // specifies start of script definition
-	create_write_byte(13);         // specifies number of bytes to follow,(defining the script)
-	create_write_byte(137);        // drive command (speed and turn radius in next 4 bytes)
-	create_write_byte(speed >> 8); // send speed high byte (bits 8-15 shifted to 0-7)
-	create_write_byte(speed);      // send speed low byte
-	create_write_byte(128);        // send hex 80
-	create_write_byte(0);          // send hex 00 (special case: turn radius hex 8000 or 7FFF is straight)
-	create_write_byte(156);        // wait for distance done (in mm)
-	create_write_byte(dist >> 8);  // send dist high byte
-	create_write_byte(dist);       // send dist low byte
-	create_write_byte(137);        // stop move by changing speed and radius to 0
-	create_write_byte(0);          // send high byte (0 speed)
-	create_write_byte(0);          // send low byte (0 speed)
-	create_write_byte(0);          // null turn radius
-	create_write_byte(0);          // null turn radius
-	// end of script (13 bytes)
-	create_write_byte(153);        //start script
-}
-
 void drive(int dist, int speed) {  //dist in mm, speed in mm/s   invertiert fuer create roboter
 	dist = dist * -1;
 	speed = speed * -1;
@@ -57,4 +17,37 @@ void drive(int dist, int speed) {  //dist in mm, speed in mm/s   invertiert fuer
 	create_write_byte(0);          // null turn radius
 	create_write_byte(0);          // null turn radius // end of script (13 bytes)
 	create_write_byte(153);        //start script
+}
+
+void drive_wait(int dist, int speed) {  //dist in mm, speed in mm/s   invertiert fuer create roboter
+	drive(int dist, int speed);
+	while(get_create_distance()!=0){} //Warten bis create fertig ist
+}
+
+void rotate(int speed, int angle) {  //angle: positiv --> rechts     negativ --> links
+	int rad = 0xFFFF;
+	if(angle<0) {
+		rad = 0x0001;
+	}
+	create_write_byte(152);        // specifies start of script definition
+	create_write_byte(13);         // specifies number of bytes to follow,(defining the script)
+	create_write_byte(137);        // drive command (speed and turn radius in next 4 bytes)
+	create_write_byte(speed >> 8); // send speed high byte (bits 8-15 shifted to 0-7)
+	create_write_byte(speed);      // send speed low byte
+	create_write_byte(rad >> 8);        // send radius high byte (bits 8-15 shifted to 0-7)
+	create_write_byte(rad);          // send radius low byte
+	create_write_byte(157);        // wait for distance done (in mm)
+	create_write_byte(angle >> 8);  // send dist high byte
+	create_write_byte(angle);       // send dist low byte
+	create_write_byte(137);        // stop move by changing speed and radius to 0
+	create_write_byte(0);          // send high byte (0 speed)
+	create_write_byte(0);          // send low byte (0 speed)
+	create_write_byte(0);          // null turn radius
+	create_write_byte(0);          // null turn radius // end of script (13 bytes)
+	create_write_byte(153);        //start script
+}
+
+void rotate_wait(int speed, int angle) {
+	rotate(int speed, int angle);
+	while(get_create_normalized_angle()!=0){} //Warten bis create fertig ist
 }
--- create/firstcreateprogramm.c
+++ create/firstcreateprogramm.c
@@ -1,7 +1,3 @@
-
-void turn_right(int angle);
-void turn_left(int angle);
-
 //servos
 #define cubeClaw 0
 #define botguyClaw 1
@@ -15,13 +11,13 @@ void turn_left(int angle);
 //motoren
 #define motor 1 
 #define speed 500
+#define rotate_speed 250
 
 //
 #define line_sensor_left 0
 #define line_sensor_right 1
 
-
-void main(){
+void main() {
 	enable_servos();
 	set_servo_position(leftHook, hookDown);
 	set_servo_position(rightHook, hookDown);
@@ -29,43 +25,15 @@ void main(){
 	create_connect();
 	create_full();
 	printf("start");
-	turn_right(70);
-	printf("\n jeztz nach hinten");
-	create_stop();msleep(100);
-	set_create_distance(20);
-	create_drive_straight(-100);
-	while(get_create_distance()>=0){
-	}
-	create_stop();msleep(100);
+	rotate_wait(rotate_speed,65);
+	printf("\n jetzt nach hinten");
+	drive_wait(-100,-20);
 	printf("jetzt nach vorne");
-	set_create_distance(0);
-	create_drive_straight(speed);
-	while(get_create_distance()<500){
-	}
-	create_stop();msleep(100);
-	printf("jeztz nach links");
-	turn_right(65);
+	drive_wait(speed, 500);
+	printf("jetzt nach links");
+	rotate_wait(rotate_speed,65);
 	set_servo_position(cubeClaw,claw_opend);
-	create_drive_straight(-speed);
-	set_create_distance(0);
-	while(get_create_distance()>-200){
-	}
-	create_stop();msleep(100);
+	drive_wait(-speed, -200);
 	set_servo_position(cubeClaw,claw_closed);
 	printf("done");
-	
-}
-
-void turn_right(int angle) {
-	set_create_normalized_angle(359);
-	create_spin_CW(250);
-	while(get_create_normalized_angle()>360-angle){
-	}		
-}
-
-void turn_left(int angle) {
-set_create_normalized_angle(0);
-	create_spin_CCW(250);
-	while(get_create_normalized_angle()<angle-10){
-	}		
 }
