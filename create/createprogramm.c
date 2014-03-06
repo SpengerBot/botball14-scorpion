93338c66614af8ecbb3a53e048f96f73df1f19d7
Alexander Halbarth <alexander@halbarth.at>
2014-03-06 19:02:11 +0100

    Motor für BotGuy heben implementiert

--- create/createprogramm.c
+++ create/createprogramm.c
@@ -1,10 +1,8 @@
-
-void botguyUp();
-void BotguyDown();
+void botguy_up();
+void botguy_down();
 
 //servos
 #define cubeClaw 0
-#define botguyClawLifter 0
 #define botguyClaw 2
 #define cubeClawOpen 1000
 #define cubeClawClosed 1900
@@ -18,7 +16,7 @@ void BotguyDown();
 #define hookUp 1650
 
 //motoren
-#define motor1 1 
+#define botguyMotor 0
 #define speed 500
 #define rotate_speed 250
 
@@ -26,30 +24,25 @@ void BotguyDown();
 #define BotguyClawIsUp 14
 
 void main() {
-	while(digital(BotguyClawIsUp)==0){}
-	motor(botguyClawLifter,-10000000);
-	while(digital(BotguyClawIsUp)==0){}
-	motor(botguyClawLifter,40);
-	msleep(900);
-	while(digital(BotguyClawIsUp)==0){}
-	motor(botguyClawLifter,0);
-	set_servo_position(leftHook, hookDown);
-	set_servo_position(rightHook, hookDown);
+	//set_servo_position(leftHook, hookDown);
+	//set_servo_position(rightHook, hookDown);
 	set_servo_position(cubeClaw,cubeClawClosed);
+	set_servo_position(botguyClaw,botguyClawClosed);
 	enable_servos();
 	create_connect();
 	create_full();
-	printf("Initial Setup");
+	printf("CREATE Connected :)\n");
+	botguy_up();
 	rotate_wait(rotate_speed,-90);
-	printf("wait_for_light();");//wait_for_light();
+	printf("wait_for_light();\n");//wait_for_light();
 	msleep(2000);
-	printf("start");
+	printf("start\n");
 	rotate_wait(rotate_speed,80);
-	printf("\njetzt nach hinten");
+	printf("jetzt nach hinten\n");
 	drive_wait(-speed/2,-20);
-	printf("jetzt nach vorne");
+	printf("jetzt nach vorne\n");
 	drive_wait(speed, 540);
-	printf("jetzt nach links");
+	printf("jetzt nach links\n");
 	rotate_wait(rotate_speed,85);
 	set_servo_position(cubeClaw,cubeClawOpen);
 	drive_wait(-speed, -200);
@@ -61,19 +54,30 @@ void main() {
 	rotate_wait(rotate_speed,80);
 	printf("Botguy holen\n");
 	drive_wait(speed, 100);
-	//set_servo_position(botguyClawLifter,botguyClawLifterDown);
-	//set_servo_position(botguyClaw,botguyClawOpen);
+	set_servo_position(botguyClaw,botguyClawOpen);
+	botguy_down();
 	drive_wait(-speed, -110);
 	msleep(400);
 	set_servo_position(botguyClaw,botguyClawClosed+0);
 	printf("I have got the BUTTGAY!\n"); 
 	msleep(700);
-  // set_servo_position(botguyClawLifter,botguyClawLifterUp);
+	botguy_up();
 	rotate_wait(rotate_speed,185);
-		drive_wait(speed, 70);
+	drive_wait(speed, 70);
 	printf("done");
 	msleep(1000);
 	create_disconnect();
 	
 }
 
+void botguy_up() {
+	motor(botguyMotor,100);
+	while(!digital(BotguyClawIsUp)){}
+	motor(botguyMotor,0);
+}
+
+void botguy_down() {
+	motor(botguyMotor,-40);
+	msleep(700);
+	motor(botguyMotor,10);
+}
