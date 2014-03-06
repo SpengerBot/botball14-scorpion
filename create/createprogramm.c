c7fe09c41058c5811fd3b6d91017baf36f2e84a7
Michael Kaufmann <michi.kaufmann1012@gmail.com>
2014-03-06 17:42:20 +0100

    Nicht funktionierendes motordrehen des botguyhebers alex is schuld.... entenfüße

--- create/createprogramm.c
+++ create/createprogramm.c
@@ -1,14 +1,15 @@
 
-void closeClaw();
-void openClaw();
+void botguyUp();
+void BotguyDown();
+
 //servos
 #define cubeClaw 0
 #define botguyClawLifter 0
 #define botguyClaw 2
+#define cubeClawOpen 1000
+#define cubeClawClosed 1900
 #define botguyClawOpen 660
 #define botguyClawClosed 1500
-#define botguyClawLifterUp 1380
-#define botguyClawLifterDown 170
 //hooks
 #define leftHook 3
 #define rightHook 1
@@ -21,15 +22,20 @@ void openClaw();
 #define speed 500
 #define rotate_speed 250
 
-//
-#define line_sensor_left 0
-#define line_sensor_right 1
+//sensors
+#define BotguyClawIsUp 14
 
 void main() {
+	while(digital(BotguyClawIsUp)==0){}
+	motor(botguyClawLifter,-10000000);
+	while(digital(BotguyClawIsUp)==0){}
+	motor(botguyClawLifter,40);
+	msleep(900);
+	while(digital(BotguyClawIsUp)==0){}
+	motor(botguyClawLifter,0);
 	set_servo_position(leftHook, hookDown);
 	set_servo_position(rightHook, hookDown);
-	set_servo_position(botguyClawLifter,botguyClawLifterUp);
-	set_servo_position(botguyClaw,botguyClawClosed);
+	set_servo_position(cubeClaw,cubeClawClosed);
 	enable_servos();
 	create_connect();
 	create_full();
@@ -45,44 +51,29 @@ void main() {
 	drive_wait(speed, 540);
 	printf("jetzt nach links");
 	rotate_wait(rotate_speed,85);
-	openClaw();
+	set_servo_position(cubeClaw,cubeClawOpen);
 	drive_wait(-speed, -200);
 	msleep(200);
-	closeClaw();
+	set_servo_position(cubeClaw,cubeClawClosed);
 	printf("I have got the cube!\n");
 	drive_wait(-speed, -100);
 	printf("nach rechts\n");
 	rotate_wait(rotate_speed,80);
 	printf("Botguy holen\n");
 	drive_wait(speed, 100);
-	set_servo_position(botguyClawLifter,botguyClawLifterDown);
-	set_servo_position(botguyClaw,botguyClawOpen);
+	//set_servo_position(botguyClawLifter,botguyClawLifterDown);
+	//set_servo_position(botguyClaw,botguyClawOpen);
 	drive_wait(-speed, -110);
 	msleep(400);
 	set_servo_position(botguyClaw,botguyClawClosed+0);
 	printf("I have got the BUTTGAY!\n"); 
 	msleep(700);
-    set_servo_position(botguyClawLifter,botguyClawLifterUp);
+  // set_servo_position(botguyClawLifter,botguyClawLifterUp);
 	rotate_wait(rotate_speed,185);
 		drive_wait(speed, 70);
 	printf("done");
 	msleep(1000);
 	create_disconnect();
+	
+}
 
-}
-void closeClaw(){
-	motor(cubeClaw,10);
-	printf("\nzu");
-	msleep(300);
-	printf("\nwartZu");
-	off(cubeClaw);
-}
-void openClaw(){
-	motor(cubeClaw,-80);
-	msleep(300);
-	motor(cubeClaw,-30);
-	printf("\nauf");
-	msleep(450);
-	printf("\nwartAuf");
-	off(cubeClaw);
-}
