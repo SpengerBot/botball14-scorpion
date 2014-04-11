7003d2247378941a1721b21e0ec5e1c0ebe379af
Alexander Halbarth <alexander@halbarth.at>
2014-04-11 09:53:54 +0200

    New WORKING wait_for_light method

--- Scorpion-2/wait_for_light.c
+++ Scorpion-2/wait_for_light.c
@@ -1,49 +1,61 @@
+#include <kovan/kovan.h>
+ 
 void wait_for_light_save(int light_port_)
 {
-	int l_on_, l_off_, l_mid_, OK=0;
-
+	int xBut, l_on_, l_off_, l_mid_, OK = 0;
+	xBut = get_extra_buttons_visible();
+	set_extra_buttons_visible(0);
+	set_a_button_text("-");
+	set_c_button_text("-");
+	set_analog_pullup(light_port_, 1);
 	while (!OK) {
-		cbc_display_clear();
-		cbc_printf (0,0,"CALIBRATE: sensor port #%d", light_port_);
-		sleep(1.); sleep(1.);
-
-		cbc_printf(0,1,"  press <-- when light on");
-		while(!left_button()){/*sensor value when light is on*/
-			cbc_printf(0,2,"  value is %d, bright = low   ",l_on_=analog10 (light_port_)); 
+		set_b_button_text("Light is On");
+		display_clear();
+		display_printf (0, 0, "CALIBRATE: sensor port #%d", light_port_);
+		display_printf(0, 1, "   press button when light is on");
+		while(b_button_clicked() == 0) {
+			l_on_ = analog (light_port_);
+			display_printf(0,1,"   light on value is = %d        ", l_on_);
 			msleep(50);
 		}
-
-		cbc_printf(0,1,"  light on value is = %d        ", l_on_);
-		sleep(1.);
-
-		cbc_printf(0,2,"  press --> when light off             ");
-		while(!right_button()){	/*sensor value when light is off*/
-			cbc_printf(0,3,"   value is %d, dark = high   ",l_off_=analog10 (light_port_)); 
+		l_on_ = analog(light_port_); /* sensor value when light is on */
+ 
+		set_b_button_text("Light is Off");
+ 
+		display_printf(0,1,"   light on value is = %d        ", l_on_);
+		msleep(200);
+ 
+		display_printf(0,2,"   press button when light off");
+		while(b_button_clicked() == 0) {
+			l_off_ = analog(light_port_);
+			display_printf(0,3,"   light off value is = %d         ", l_off_);
 			msleep(50);
 		}
-
-		cbc_printf(0,2,"  light off value is = %d         ", l_off_);
-		sleep(1.);
-
-		cbc_printf(0,3,"                              ");
-
-		if((l_off_-l_on_)>=120){ /*bright = small values */
-			OK=1;
-			l_mid_=(l_on_+l_off_)/2;
-			cbc_printf(0,4,"Good Calibration!");
-			cbc_printf(0,6,"Diff = %d:  WAITING",l_off_-l_on_);
-			int on = 0;									//New Code to avoid flashlight starting!
-			while(analog10(light_port_)>l_mid_ || !on){		//New Code to avoid flashlight starting!
-				cbc_printf(0,7,"Value = %d; Threshold = %d   ",analog10(light_port_),l_mid_);
-				msleep(12);
-				on = analog10(light_port_)>l_mid_;		//New Code to avoid flashlight starting!
-				msleep(12);								//New Code to avoid flashlight starting!
+		l_off_ = analog(light_port_); /* sensor value when light is off */
+ 
+		display_printf(0,3,"   light off value is = %d         ", l_off_);
+		msleep(200);
+ 
+		if((l_off_ - l_on_) >= 60) { /* bright = small values */
+			OK = 1;
+			l_mid_ = (l_on_ + l_off_) / 3;
+			display_printf(0, 5, "Good Calibration!");
+			display_printf(0, 7, "Diff = %d:  WAITING", l_off_ - l_on_);
+			int start = 0;
+			while(!start){
+				while(analog(light_port_) > l_mid_);
+				msleep(250);
+				if(analog(light_port_) < l_mid_){
+					start = 1;
+				}
 			}
-			cbc_printf(0,6,"Going!                      ");
-			cbc_printf(0,7,"Value = %d; Threshold = %d   ",analog10(light_port_),l_mid_);
-		}
-		else{
-			cbc_printf(0,6,"BAD CALIBRATION");
+			
+		} else {
+			display_printf(0,7,"BAD CALIBRATION");
 		}
 	}
+	set_extra_buttons_visible(xBut);
+	set_a_button_text("A");
+	set_b_button_text("B");
+	set_c_button_text("C");
 }
